7454764a3c2ed174fb1f187e76075a57776f7c01
Gosia82 <majg.tychy@gmail.com>
2021-05-19 18:11:51 +0200

    [change] - interface and data sql

---
+++ src/main/java/pl/sda/mybudget/repository/IncomeRepository.java
@@ -0,0 +1,5 @@
+package pl.sda.mybudget.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.sda.mybudget.model.Income;
+public interface IncomeRepository extends JpaRepository<Income, Long> {
+}
---
+++ src/main/java/pl/sda/mybudget/service/IncomeService.java
@@ -0,0 +1,11 @@
+package pl.sda.mybudget.service;
+
+import pl.sda.mybudget.model.Income;
+
+import java.util.List;
+
+public interface IncomeService {
+
+
+    List<Income> findAllIncomes();
+}
